8dfb200d23b0f9250c6cd48af9a8c2ef615af102
fediushkin <fediushkin@fim.uni-passau.de>
2021-11-27 18:35:09 +0100

    initial commit

---
+++ src/jobscraping/views.py
@@ -0,0 +1,9 @@
+import datetime
+from django.shortcuts import render
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'Dave'
+    _context = {'date':date,
+                'name': name}
+    return render(request, 'home.html', _context)
